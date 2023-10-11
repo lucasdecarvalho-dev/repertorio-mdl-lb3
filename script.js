7e0646257f7531ee6055c1a8b87bcf774d6aabc1
lucasdecarvalho-dev <luckassilva.ls928@gmail.com>
2023-10-11 12:15:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const adoracao = [
+  "É tudo sobre você - Morada",
+  "Quero conhecer Jesus - Alessandro Vilas Boas",
+  "Pode morar aqui - Theo Rubia"
+];
+
+const celebracao = [
+  "No meio dos louvores - Israel Salazar",
+  "Medley - Morada",
+  "Somos um - Salzband"
+]
+
+const vitoria = [
+  "Você não vai parar - Samuel Messias",
+  "Fidelidade - Daniele Cristina",
+  "Identidade - Anderson Freire"
+]
